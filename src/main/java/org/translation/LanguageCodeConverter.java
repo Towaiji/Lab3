15f818242a3d440e7c13e302f92c719347bbf6f2
Towaiji <aliyasiraltowaiji@gmail.com>
2024-09-27 11:26:37 -0400

    codeconverter

--- src/main/java/org/translation/LanguageCodeConverter.java
+++ src/main/java/org/translation/LanguageCodeConverter.java
@@ -5,6 +5,7 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -13,7 +14,8 @@ import java.util.Map;
  */
 public class LanguageCodeConverter {
 
-    // TODO Task: pick appropriate instance variables to store the data necessary for this class
+    private Map<String, String> codeToLanguage;
+    private Map<String, String> languageToCode;
 
     /**
      * Default constructor which will load the language codes from "language-codes.txt"
@@ -29,13 +31,32 @@ public class LanguageCodeConverter {
      * @throws RuntimeException if the resource file can't be loaded properly
      */
     public LanguageCodeConverter(String filename) {
-
+        codeToLanguage = new HashMap<>();
+        languageToCode = new HashMap<>();
         try {
             List<String> lines = Files.readAllLines(Paths.get(getClass()
                     .getClassLoader().getResource(filename).toURI()));
 
-            // TODO Task: use lines to populate the instance variable
-            //           tip: you might find it convenient to create an iterator using lines.iterator()
+            Iterator<String> iterator = lines.iterator();
+            if (iterator.hasNext()) {
+                iterator.next();
+            }
+
+            while (iterator.hasNext()) {
+                String line = iterator.next().trim();
+                if (line.isEmpty()) {
+                    continue;
+                }
+                String[] split = line.split("\\s");
+                if (split.length < 2) {
+                    continue;
+                }
+                String language = String.join(" ", java.util.Arrays.copyOf(split, split.length - 1));
+                String code = split[split.length - 1].trim();
+
+                codeToLanguage.put(code, language);
+                languageToCode.put(language, code);
+            }
 
         }
         catch (IOException | URISyntaxException ex) {
@@ -50,8 +71,7 @@ public class LanguageCodeConverter {
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task: update this code to use your instance variable to return the correct value
-        return code;
+        return codeToLanguage.getOrDefault(code, "Language not found");
     }
 
     /**
@@ -60,8 +80,7 @@ public class LanguageCodeConverter {
      * @return the 2-letter code of the language
      */
     public String fromLanguage(String language) {
-        // TODO Task: update this code to use your instance variable to return the correct value
-        return language;
+        return languageToCode.getOrDefault(language, "Code not found");
     }
 
     /**
@@ -69,7 +88,6 @@ public class LanguageCodeConverter {
      * @return how many languages are included in this code converter.
      */
     public int getNumLanguages() {
-        // TODO Task: update this code to use your instance variable to return the correct value
-        return 0;
+        return codeToLanguage.size();
     }
 }
